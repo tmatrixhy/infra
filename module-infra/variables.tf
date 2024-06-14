088f217e0dd4c6297292c6e311afa7f8f8201db9
tmatrixhy <xitram@gmail.com>
2024-06-14 16:54:37 -0400

    feat: basic setup n start

---
+++ module-infra/variables.tf
@@ -0,0 +1,11 @@
+variable "k3d_cluster_name" {
+  default     = "base-infra"
+}
+
+variable "k3d_api_server" {
+  default     = "localhost"
+}
+
+variable "k3d_server_port" {
+  default     = 6443
+}
