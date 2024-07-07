9bae7a44e1164c3fab9222a7cd632f1c0d7b1469
tmatrixhy <xitram@gmail.com>
2024-07-07 15:13:26 -0400

    some cleanup and structure changes

--- core-cloud/100-droplet.tf
+++ core-cloud/100-droplet.tf
@@ -6,6 +6,7 @@ resource "digitalocean_ssh_key" "generated_key" {
 }
 
 resource "digitalocean_droplet" "primary_server" {
+  depends_on = [ null_resource.delete_ssh_key ]
   name   = var.droplet_name
   size   = var.droplet_size
   image  = var.droplet_image
@@ -22,31 +23,4 @@ resource "digitalocean_droplet" "primary_server" {
     [ digitalocean_ssh_key.generated_key.fingerprint ],
     var.additional_droplet_ssh_keys
   )              
-}
-
-resource "null_resource" "run_remote_commands" {
-  depends_on = [ digitalocean_droplet.primary_server, 
-                tailscale_tailnet_key.primary_server ]
-  count = var.ssh_private_key_path != "" ? 1 : 0
-
-  connection {
-    type        = "ssh"
-    user        = "root"
-    private_key = file("${var.ssh_private_key_path}/${random_id.key.hex}")
-    host        = local.full_domain_name
-  }
-  
-  provisioner "file" {
-    source      = var.bootstrap_script
-    destination = "/tmp/bootstrap.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "curl -fsSL https://tailscale.com/install.sh | sh",
-      "tailscale up --authkey=${tailscale_tailnet_key.primary_server.key}",
-      "chmod +x /tmp/bootstrap.sh",
-      "for i in {1..3}; do /tmp/bootstrap.sh && break || sleep 15; done"
-    ]
-  }
 }
--- core-cloud/droplet-bootstrap.sh
+++ core-cloud/droplet-bootstrap.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo 'Hello, World!' > /root/hello.txt
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
-sleep 15
-apt-get upgrade -y
+apt-get update && apt-get upgrade -y
+apt-get install ansible -y
